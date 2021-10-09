17412f3d00fba296d799cedca41fd968bea8eba8
Amit <amitgaikwad795@gmail.com>
2021-10-09 16:50:15 +0530

    add mongodb data source

---
+++ src/models/student.model.ts
@@ -0,0 +1,36 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Student extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+  })
+  name?: string;
+
+  @property({
+    type: 'number',
+  })
+  mobile?: number;
+
+  @property({
+    type: 'string',
+  })
+  city?: string;
+
+
+  constructor(data?: Partial<Student>) {
+    super(data);
+  }
+}
+
+export interface StudentRelations {
+  // describe navigational properties here
+}
+
+export type StudentWithRelations = Student & StudentRelations;
